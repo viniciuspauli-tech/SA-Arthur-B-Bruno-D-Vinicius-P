20c90e2dce0a0f8b64dc1b62cc837f3d0d249ff3
bruno <bruno_d_luiz@estudante.sesisenai.org.br>
2026-09-09 12:51:25 -0300

    add - data base

---
+++ data base/banco.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE IF NOT EXISTS ferrorama
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+USE ferrorama;
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE trens (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    identificacao VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE sensores (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    localizacao VARCHAR(150) NOT NULL,
+    tipo_dado VARCHAR(100) NOT NULL,
+    trem_id INT NOT NULL,
+
+    FOREIGN KEY (trem_id)
+        REFERENCES trens(id)
+);
